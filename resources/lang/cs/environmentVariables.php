3bfe094b3e57a7a3ec80e27cf81445c415467896
Jan Rosa <jan.rosa@freshflow.cz>
2021-08-08 23:03:33 +0200

    NFF-1705 translation files

---
+++ resources/lang/cs/environmentVariables.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "validation" => [
+        "name" => [
+            "invalid_variable_name" => "Jméno musí začít písmenem a může obsahovat pouze písmena, čísla a podtržítko (_).",
+        ],
+    ],
+];
